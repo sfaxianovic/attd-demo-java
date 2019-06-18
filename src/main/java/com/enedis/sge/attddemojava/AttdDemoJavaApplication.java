66c12b09b5632dee7cf42eda59d9ff22f03326d8
asfaxi <asfaxi@platana.fr>
2019-06-18 23:21:28 +0200

    Initial commit

---
+++ src/main/java/com/enedis/sge/attddemojava/AttdDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.enedis.sge.attddemojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttdDemoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttdDemoJavaApplication.class, args);
+	}
+
+}
